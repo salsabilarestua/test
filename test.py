55ee0f2a46f008e441d0781f722f17b6620fe8fd
richie <ikhsannul201@gamil.com>
2024-11-11 10:50:25 +0700

    fungsi login

---
+++ test.py
@@ -0,0 +1,65 @@
+import json
+import os
+import getpass
+
+
+DATA_FILE = 'user_data.json'
+
+def load_user_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r') as file:
+            return json.load(file)
+    return {}
+
+def save_user_data(user_data):
+    with open(DATA_FILE, 'w') as file:
+        json.dump(user_data, file, indent=4)
+
+def register():
+    username = input("Masuukkan username: ")
+    password = getpass.getpass("Masukkan password baru: ")
+
+    
+    user_data = load_user_data()
+    
+    if username in user_data:
+        print("Usernam sudah ada. Silakan coba login.")
+        return
+
+    
+    user_data[username] = password
+    save_user_data(user_data)
+    print("Berhasil Register.")
+
+def login():
+    
+    username = input("Masuukkan username: ")
+    password = getpass.getpass("Masukkan password baru: ")
+
+    
+    user_data = load_user_data()
+    
+    if username in user_data and user_data[username] == password:
+        print("Login Berhasil!")
+    else:
+        print("Username atau Password berhasil.")
+
+def main():
+    """Main function to run the CLI login system."""
+    while True:
+        print("\n1. Register")
+        print("2. Login")
+        print("3. Exit")
+        choice = input("Pilih dengan angka: ")
+
+        if choice == '1':
+            register()
+        elif choice == '2':
+            login()
+        elif choice == '3':
+            break
+        else:
+            print("Salah input. Silakan coba lagi.")
+
+if __name__ == "__main__":
+    main()
