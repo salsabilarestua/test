c58cdc790d1245a9ac8401398f32600b384a072f
richie <ikhsannul201@gamil.com>
2024-11-14 23:01:35 +0700

    penambahan fungsi save tips

---
+++ function/tipsBelajar.py
@@ -0,0 +1,52 @@
+import json
+import os
+import time
+import utils.helper as helper
+
+DATA_FILE = 'tips_belajar.json'
+
+def load_tips_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r') as file:
+            return json.load(file)
+    return{}
+
+def save_tips_data(tips_data):
+    with open(DATA_FILE, 'w') as file:
+        json.dump(tips_data, file, indent=4)
+
+def tambahTips():
+    admin = input("Masukkan nama anda: ")
+    tips = input("Masukkan Tips: ")
+    
+    tips_data = load_tips_data()
+
+    if tips in tips_data:
+        print("\nTips sudah ada! Silakan masukkan tips kembali.")
+        time.sleep(1)
+        return
+    
+    tips_data[admin] = tips
+    save_tips_data(tips_data)
+    print("Berhasil menyimpan tips. ") 
+
+def main():
+    """Main function to run the CLI login system."""
+    while True:
+        helper.clear()
+        print("Silahkan melakukan login terlebih dahulu!\n")
+        print("1. Tambah tips")
+        print("2. exit")
+        choice = input("Pilih dengan angka: ")
+
+        if choice == '1':
+            helper.clear()
+            tambahTips()
+        elif choice == '2':
+            break
+        else:
+            print("\nInvalid input! Silakan coba lagi.")
+            time.sleep(1)
+
+if __name__ == "__main__":
+    main()
